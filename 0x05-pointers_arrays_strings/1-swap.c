467cbf663367bc8eb166c55d6474eab41613e4a9
UwemUnak <anthonyunak@gmail.com>
2022-10-24 12:02:08 -0700

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
-#include "holberton.h" 
-  
- /** 
-  * swap_int - swaps the values of a and b 
-  * @a: pointer 1 
-  * @b: pointer 2 
-  * Return; 0 
-  */ 
-  
- void swap_int(int *a, int *b) 
- { 
-         *a += *b; 
-         *b = *a - *b; 
-         *a = *a - *b; 
- }
+#include "main.h"                                                                                                                                                         
+                                                                                                                                                                          
+/**                                                                                                                                                                       
+ * swap_int - swaps the values of two integers                                                                                                                            
+ * @a: int a                                                                                                                                                              
+ * @b: int b                                                                                                                                                              
+ */                                                                                                                                                                       
+void swap_int(int *a, int *b)                                                                                                                                             
+{                                                                                                                                                                         
+        int q;                                                                                                                                                            
+                                                                                                                                                                          
+        q = *a;                                                                                                                                                           
+        *a = *b;                                                                                                                                                          
+        *b = q;                                                                                                                                                           
+}
+
