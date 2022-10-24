ab69958d5be1cda9aacc1630ac857c5454153dee
UwemUnak <anthonyunak@gmail.com>
2022-10-24 14:15:59 -0700

    puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,7 +5,6 @@
  * @str: input string.
  * Return: no return.
  */
-
 void puts_half(char *str)
 {
 	int count = 0, i;
@@ -23,5 +22,5 @@ void puts_half(char *str)
 
 	for (i++; i < count; i++)
 		_putchar(str[i]);
- _putchar('\n');
+_putchar('\n');
 }
